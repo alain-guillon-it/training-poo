8a19d33471967ff4c4c6c7305f9259304a947b83
Alain Guillon <a.guillon@it-students.fr>
2022-12-31 16:45:54 +0100

    Ajout des boucles pour récupérer la data pour la page d'accueil

--- public/views/home/home.view.php
+++ public/views/home/home.view.php
@@ -5,28 +5,64 @@
   <section>
     <h2>Three last movies</h2>
     <div>
-      <?php dump($page->getLastThreeMovies()); ?>
+      <?php foreach ($page->getLastThreeMovies() as $movie) : ?>
+        <a href=<?= "/?page=movies&action=detail&id=" . $movie["id"]; ?>>
+          <figure>
+            <img src=<?= $movie["cover"]; ?> alt=<?= "The cover of " . $movie["title"] . " movie."; ?>>
+            <figcaption>
+              <?= $movie["title"]; ?>
+            </figcaption>
+          </figure>
+        </a>
+      <?php endforeach; ?>
     </div>
   </section>
 
   <section>
     <h2>Three last series</h2>
     <div>
-      <?php dump($page->getLastThreeSeries()); ?>
+      <?php foreach ($page->getLastThreeSeries() as $serie) : ?>
+        <a href=<?= "/?page=series&action=detail&id=" . $serie["id"] ?>>
+          <figure>
+            <img src=<?= $serie["cover"]; ?> alt=<?= "The cover of " . $serie["title"] . " serie." ?>>
+            <figcaption>
+              <?= $serie["title"]; ?>
+            </figcaption>
+          </figure>
+        </a>
+      <?php endforeach; ?>
     </div>
   </section>
 
   <section>
     <h2>Three last actors</h2>
     <div>
-      <?php dump($page->getLastThreeActors()); ?>
+      <?php foreach ($page->getLastThreeActors() as $actor) : ?>
+        <a href=<?= "/?page=actors&action=detail&id=" . $actor["id"]; ?>>
+          <figure>
+            <img src=<?= $actor["photo"]; ?> alt=<?= "The cover of " . $actor["fullname"] . " movie."; ?>>
+            <figcaption>
+              <?= $actor["fullname"]; ?>
+            </figcaption>
+          </figure>
+        </a>
+      <?php endforeach; ?>
     </div>
   </section>
 
   <section>
     <h2>Three last realisators</h2>
     <div>
-      <?php dump($page->getLastThreeRealisators()); ?>
+      <?php foreach ($page->getLastThreeRealisators() as $realisator) : ?>
+        <a href=<?= "/?page=realisators&action=detail&id=" . $realisator["id"]; ?>>
+          <figure>
+            <img src=<?= $realisator["photo"]; ?> alt=<?= "The photo of " . $actor["fullname"] . " realisator."; ?>>
+            <figcaption>
+              <?= $realisator["fullname"]; ?>
+            </figcaption>
+          </figure>
+        </a>
+      <?php endforeach; ?>
     </div>
   </section>
 
