13461a053ed9b3cd0bdead4e48b8cd20ef84e468
Alain Guillon <a.guillon@it-students.fr>
2023-01-06 14:45:59 +0100

    update a css for an error page

--- public/views/error/error.view.php
+++ public/views/error/error.view.php
@@ -1,4 +1,6 @@
-<article class="error">
+<article class="article__error">
   <h1>Page 404</h1>
-  <p>Désolé la page que vous avez demandé n'existe pas.</p>
+  <p>Désolé dans l'url quelque chose n'a pas bien marché...<br />
+    Vous avez saisie : <strong><?= $_SERVER["REQUEST_URI"]; ?></strong>.</p>
+  <p>Veuillez rectifier votre saisie mercie.</p>
 </article>
